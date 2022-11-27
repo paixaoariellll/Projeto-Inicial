247723f3a1699b0d339a3724d828d779177159b8
paixaoariellll <paixaoariellll@gmail.com>
2022-11-27 15:41:09 -0300

    projeto inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>CHAT APP</title>
+      </Head>
+      <div> ta funfando</div>
+    </div>
+  )
+}
