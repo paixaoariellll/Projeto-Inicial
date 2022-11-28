18169ee64e769d01352769568cc8f41546bc13a1
paixaoariellll <paixaoariellll@gmail.com>
2022-11-27 21:31:11 -0300

    searchBar

---
+++ components/DropdownHeader.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { IconButton } from '@mui/material';
+export default function PositionedMenu() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <>
+            <IconButton
+                onClick={handleClick}
+            >
+                <MoreVertIcon />
+            </IconButton>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                <MenuItem onClick={handleClose}>Novo Grupo</MenuItem>
+                <MenuItem onClick={handleClose}>Criar uma Sala</MenuItem>
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>Conversas Arquivadas</MenuItem>
+                <MenuItem onClick={handleClose}>Mensagens Favoritas</MenuItem>
+                <MenuItem onClick={handleClose}>Configurações</MenuItem>
+                <MenuItem onClick={handleClose}>Sair</MenuItem>
+            </Menu>
+        </>
+    );
+}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import { Avatar, IconButton } from '@mui/material';
+import MessageIcon from '@mui/icons-material/Message';
+import VerticalItens from './DropdownHeader';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Sidebar = () => {
     return (
@@ -8,10 +11,17 @@ const Sidebar = () => {
                 <UserAvatar src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
                 <IconsGroup>
                     <IconButton>
-                        <img src="/story.svg"></img>
+                        <MessageIcon />
                     </IconButton>
+                    <VerticalItens />
                 </IconsGroup>
             </Header>
+            <SearchChat >
+                <SearchBar>
+                    <SearchIcon />
+                    <SearchInput />
+                </SearchBar>
+            </SearchChat>
         </Container>
     )
 }
@@ -19,7 +29,7 @@ const Sidebar = () => {
 export default Sidebar
 
 const Container = styled.div`
-    backgroud: #fff;
+    backgroud: #ffffff;
     min-width: 320px;
     max-width: 450px;
     height: 100%;
@@ -47,4 +57,24 @@ const UserAvatar = styled(Avatar)`
 `;
 
 const IconsGroup = styled.div`
+`;
+
+const SearchChat = styled.div`
+    background-color: #f6f6f6;
+    border-bottom: 1px solid rgba(0,0,0,0.1);
+    padding: 20px;
+`;
+
+const SearchBar = styled.div`
+    display: flex;
+    padding: 5px;
+    border-radius: 10px;
+    border-bottom: 1px solid #ededed;
+    color: #000;
+    background-color: #fff;
+`;
+
+const SearchInput = styled.div`
+    width: 100%;
+    border: none;
 `;
